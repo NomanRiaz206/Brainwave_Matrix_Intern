c4e27293d61f22fac37be3ec8b4cec64a6b38efa
NomanRiaz206 <nomanriaz.dev@gmail.com>
2024-11-26 00:16:06 +0500

    New commit

--- src/Components/Logobar.jsx
+++ src/Components/Logobar.jsx
@@ -8,32 +8,35 @@ const Logobar = () => {
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       <span className="ml-3 text-3xl">SAPPHIRE</span>
     </a>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center text-black">
     <div className="mb-2 lg:w-96">
-          <div className="relative mb-4 flex w-full flex-wrap items-stretch ">
-              <input
-                  type="search"
-                  className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                  placeholder="FIND YOUR FAVOURITES"
-                  aria-label="Search"
-                  aria-describedby="button-addon2" />
+    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+  <input
+    type="search"
+    className="relative block w-full placeholder-black dark:placeholder-black rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.5rem] pr-10 text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:border-primary focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary"
+    placeholder="FIND YOUR FAVOURITES"
+    aria-label="Search"
+    aria-describedby="button-addon2"
+  />
 
-              {/* <!--Search icon--> */}
-              <span
-                  className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
-                  id="basic-addon2">
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      className="h-5 w-5">
-                      <path
-                          fillRule="evenodd"
-                          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                          clipRule="evenodd" />
-                  </svg>
-              </span>
-          </div>
+  {/* Search Icon */}
+  <span className="absolute inset-y-0 right-3 flex items-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      className="h-5 w-5 text-black dark: text-dark"
+    >
+      <path
+        fillRule="evenodd"
+        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+        clipRule="evenodd"
+      />
+    </svg>
+  </span>
+</div>
+
+
       </div>
     </nav>
     <div className="flex items-center justify-center">
